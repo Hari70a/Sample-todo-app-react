e097b5786397935ad3aba89582fe6b4a04720a87
Hari70a <raja70a@gmail.com>
2019-04-13 00:10:01 +0530

    Updates

---
+++ precache-manifest.4f7045100c2150e1158e9d1d477f282d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "03b87093c449c8921843",
+    "url": "/Sample-todo-app-react/static/js/runtime~main.e1564cb1.js"
+  },
+  {
+    "revision": "91f3db694ea38c321292",
+    "url": "/Sample-todo-app-react/static/js/main.cbfd2b22.chunk.js"
+  },
+  {
+    "revision": "a47743eae1f3be536947",
+    "url": "/Sample-todo-app-react/static/js/2.668f1759.chunk.js"
+  },
+  {
+    "revision": "91f3db694ea38c321292",
+    "url": "/Sample-todo-app-react/static/css/main.1b626cd4.chunk.css"
+  },
+  {
+    "revision": "a947f4100a8034a9db598c84db4e72ea",
+    "url": "/Sample-todo-app-react/index.html"
+  }
+];
